406ab9051cbdf6d7231a51703da83aafba6b1945
Alex531-debug <alsemyannikow@gmail.com>
2021-02-13 16:36:42 +0500

    Создал Controller для администрирования автомобилей и брендов

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,20 +2,26 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Auto;
+use App\Entity\Brand;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_dashboard")
      */
     public function index(): Response
     {
-        return parent::index();
+        $productListUrl = $this->get(CrudUrlGenerator::class)->build()->setController(UserCrudController::class)->generateUrl();
+
+        return $this->redirect($productListUrl);
     }
 
     public function configureDashboard(): Dashboard
@@ -27,6 +33,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Пользователи', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Автомобили', 'fas fa-car-side', Auto::class);
+        yield MenuItem::linkToCrud('Бренды', 'fas fa-copyright', Brand::class);
+        //yield MenuItem::linkToCrud('Бренды', 'fas fa-users', Brand::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Customer')
+            ->setEntityLabelInPlural('Customers')
+            ->setSearchFields(['id', 'email']);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield FormField::addPanel('Account Information');
+        yield IntegerField::new('id', 'ID')->onlyOnIndex();
+        yield TextField::new('name');
+        yield TextField::new('email');
+
+    }
+}
