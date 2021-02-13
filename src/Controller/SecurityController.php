85ac48ebf0bcd44675a28e819228aff26e5733a8
Alex531-debug <alsemyannikow@gmail.com>
2021-02-13 15:38:34 +0500

    Авторизация через EasyAdmin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,21 +9,29 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+
     /**
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            // parameters usually defined in Symfony login forms
+            'error' => $error,
+            'last_username' => $lastUsername,
+            'translation_domain' => 'admin',
+            'page_title' => 'ACME login',
+            'csrf_token_intention' => 'authenticate',
+            'username_label' => 'email',
+            'password_label' => 'пароль',
+            'sign_in_label' => 'Войти',
+            'username_parameter' => 'email',
+            'password_parameter' => 'password',
+        ]);
     }
 
     /**
