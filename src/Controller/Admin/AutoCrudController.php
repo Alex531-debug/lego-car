f2295f54986e88fbb1ab657c68e50e33cbdd4dd4
Alex531-debug <alsemyannikow@gmail.com>
2021-02-13 16:51:59 +0500

    Объявление полей в AutoCrudController

--- src/Controller/Admin/AutoCrudController.php
+++ src/Controller/Admin/AutoCrudController.php
@@ -7,16 +7,8 @@ namespace App\Controller\Admin;
 use App\Entity\Auto;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class AutoCrudController extends AbstractCrudController
 {
@@ -30,18 +22,11 @@ class AutoCrudController extends AbstractCrudController
     {
         return $crud
             ->setEntityLabelInSingular('Автомобиль')
-            ->setEntityLabelInPlural('Автомобили')
-            ->setSearchFields(['id', 'name', 'model', 'image', 'brand', 'price', 'vin']);
+            ->setEntityLabelInPlural('Автомобили');
     }
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            'name',
-            'model',
-            'vin',
-            'price',
-            'status',
-        ];
+        yield IntegerField::new('id', 'ID')->onlyOnIndex();
     }
 }
