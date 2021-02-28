389b3a060784f227598daba772d2beb9386b7cbd
Alex531-debug <alsemyannikow@gmail.com>
2021-02-28 21:36:31 +0500

    Добавил doctrine-test-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
